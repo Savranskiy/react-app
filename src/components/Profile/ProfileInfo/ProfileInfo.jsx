b4a4ab043fd9a16b21be7661d2252ca2719720af
Taras <taras.savranskii@outlook.com>
2020-07-31 12:33:36 +0300

    Update profile

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import cn from './ProfileInfo.module.css';
+import Preloader from '../../common/Preloader/Preloader';
 
-const ProfileInfo = () => {
+const ProfileInfo = (props) => {
+    if (!props.profile) {
+        return <Preloader />;
+    }
+
     return (
         <div>
             <div>
                 <img src="https://www.w3schools.com/howto/img_snow_wide.jpg" alt="header" />
             </div>
             <div className={cn.descriptionBlock}>
-                ava + description
+                <img src={props.profile.photos.small} />
+                <br/>
+                {props.profile.aboutMe}
                 {/* <img src="https://cdn.jpegmini.com/user/images/slider_puffin_jpegmini_mobile.jpg" alt="avatar" /> */}
             </div>
         </div>
--- src/redux/reducers/profileReducer.js
+++ src/redux/reducers/profileReducer.js
@@ -1,12 +1,14 @@
 const ADD_POST = 'ADD-POST';
 const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
+const SET_USER_PROFILE = 'SET_USER_PROFILE';
 
 const initialState = {
     posts: [
         { id: 1, message: 'Hi, how are you?', likesCount: 12 },
         { id: 2, message: 'It\'s my first post!', likesCount: 11 },
     ],
-    newPostText: ''
+    newPostText: '',
+    profile: null
 };
 
 const profileReducer = (state = initialState, action) => {
@@ -24,18 +26,16 @@ const profileReducer = (state = initialState, action) => {
                 newPostText: ''
             };
         case UPDATE_NEW_POST_TEXT:
-            return {
-                ...state,
-                newPostText: action.newText
-            };
+            return { ...state, newPostText: action.newText };
+        case SET_USER_PROFILE:
+            return { ...state, profile: action.profile }
         default:
             return state;
     }
 };
 
 export const addPostActionCreator = () => ({ type: ADD_POST });
-export const updateNewPostTextActionCreator = (text) => {
-    return { type: UPDATE_NEW_POST_TEXT, newText: text };
-}
+export const updateNewPostTextActionCreator = (text) => ({ type: UPDATE_NEW_POST_TEXT, newText: text });
+export const setUserProfile = (profile) => ({ type: SET_USER_PROFILE, profile });
 
 export default profileReducer;
